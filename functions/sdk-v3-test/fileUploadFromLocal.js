324426962fded1fbc7de67a78834b68d68c64a42
junayed-cft <hossain@vimond.com>
2024-01-03 11:44:42 +0600

    bucket name fixed

--- functions/sdk-v3-test/fileUploadFromLocal.js
+++ functions/sdk-v3-test/fileUploadFromLocal.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 const handler = async () => {
     const region = 'eu-north-1';
-    const bucket = `eu-north-1-dev-video-${org}-usp-output1`;
+    const bucket = `eu-north-1-dev-video-${org}-usp-output`;
     const folder = '8cc1f1de-cb0c-4a6b-872b-8b543e5545c0';
     const client = new S3Client({ region });
     const localFilePath = "PGM1020195881801_T3MP4800_946305.mp4";
